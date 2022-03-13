e1511e73123789ad8f03a6bc8dcd0571486b7c4b
Jose Maza <mazajose@gmail.com>
2022-03-13 22:46:54 +0100

    Initial base input component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <label>{{ label }}</label>
+  <input/>
+</template>
+
+<script>
+  export default {
+    props: {
+      label: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
