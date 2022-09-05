174e060a6641f9879a2ec014b526c956b4afc22f
Jose Maza <jose.maza@s2017.tu-chemnitz.de>
2022-09-05 12:10:27 +0200

    amazon ec2 coffee machine

--- src/components/Organisms/WotBlock.stories.js
+++ src/components/Organisms/WotBlock.stories.js
@@ -8,11 +8,13 @@ export default {
 		wotTD: {
 			name: 'wotTD',
 			type: { name: 'string', required: true },
-			defaultValue: 'http://localhost:8031/smart-coffee-machine/',
+			defaultValue: 'http://ec2-54-174-65-0.compute-1.amazonaws.com:8031/smart-coffee-machine',
 			description: 'W3C WoT Thing Description JSON.',
 		},
 	},
 };
+//http://ec2-54-174-65-0.compute-1.amazonaws.com:8032/Smart-Coffee-Machine
+//http://localhost:8037/counter/
 
 const Template = (args) => ({
 	components: { WotBlock },
